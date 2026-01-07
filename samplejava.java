2535802e6ef52d259f9c5d116e1b6e2d47f1cc57
Prudhvi-0 <2300030521@kluniversity.in>
2026-01-07 10:28:36 +0530

    Fixed class name to match file name

--- samplejava.java
+++ samplejava.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class samplejava {
     public static void main(String[] args) {
         System.out.println("Build successful! Java app built using Jenkins 🚀");
     }
